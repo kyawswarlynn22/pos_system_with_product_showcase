bd29d9c7fb3180f7ad8c7e8e73ed444f903aba35
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-10-01 23:07:30 +0630

    db migrate

---
+++ pos/database/migrations/2023_10_01_151509_create_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('c_name','256');
+            $table->string('description','256');
+            $table->integer('del_flg')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
---
+++ pos/database/migrations/2023_10_01_162155_create_sub_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('sub_c_name', '256');
+            $table->unsignedBigInteger('categories_id');
+            $table->integer('del_flg')->default(0);
+            $table->timestamps();
+            $table->foreign('categories_id')->references('id')->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+};
