e6a7a9e35f2207175c2147432f2e367c22b7ee83
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-10-02 23:25:37 +0630

    create database

---
+++ pos/database/migrations/2023_10_02_165153_create_incomes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('expense_categories_id');
+            $table->string('description',256)->nullable();
+            $table->string('date',256);
+            $table->integer('amount');
+            $table->string('photo',256)->nullable();
+            $table->integer('del_flg')->default(0);
+            $table->timestamps();
+            $table->foreign('expense_categories_id')->references('id')->on('expense_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incomes');
+    }
+};
