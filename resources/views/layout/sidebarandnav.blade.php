846e3d2fad526fdac31c678cbd46b33acb7e8c0f
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-10-22 23:43:03 +0630

    Update sidebarandnav.blade.php

--- resources/views/layout/sidebarandnav.blade.php
+++ resources/views/layout/sidebarandnav.blade.php
@@ -185,7 +185,7 @@
                         <li>
                             <a href="/product/create"
                                 class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Add
-                                Product</a>
+                                Products</a>
                         </li>
                         <li>
                             <a href="/stockadjustment"
