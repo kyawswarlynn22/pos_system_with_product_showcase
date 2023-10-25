4c344bd9571b07f35d5fb6aab34f3d958e2ca8a0
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-10-25 16:31:38 +0630

    Update sidebarandnav.blade.php

--- resources/views/layout/sidebarandnav.blade.php
+++ resources/views/layout/sidebarandnav.blade.php
@@ -11,10 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="icon" href="{{ $logo }}" sizes="16x16" type="image/png/jpeg">
-
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" defer></script>
