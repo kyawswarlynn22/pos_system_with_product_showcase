cd22d8a1c8feb935f4735e5876b12f39bc79840b
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-10-27 16:22:31 +0630

    Update sidebarandnav.blade.php

--- resources/views/layout/sidebarandnav.blade.php
+++ resources/views/layout/sidebarandnav.blade.php
@@ -245,7 +245,7 @@
                         </li>
                         <li>
                             <a href="/product"
-                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Product
+                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Stock
                                 List</a>
                         </li>
                         <li>
