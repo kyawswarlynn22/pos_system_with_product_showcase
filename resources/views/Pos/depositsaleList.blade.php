219bc139ce653ec2b42aef57d9cbb7249ac5b6c6
kyawswarlynn22 <kyawswarlynn224@gmail.com>
2023-11-09 21:39:08 +0630

    Update depositsaleList.blade.php

--- resources/views/Pos/depositsaleList.blade.php
+++ resources/views/Pos/depositsaleList.blade.php
@@ -88,7 +88,7 @@
                                     <path fill="none" d="M0 0h36v36H0z" />
                                 </svg>
                             </a>
-                            @if ($userRole == 0)
+                        
                                 <a href="/depositsale/{{ $item->id }}/edit"
                                     class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
                                     <svg width="24" height="24" viewBox="0 0 24 24"
@@ -101,7 +101,7 @@
                                         </g>
                                     </svg>
                                 </a>
-                            @endif
+                        
                             @php
                                 $timezone = 'Asia/Yangon';
                                 $currentDateTime = \Carbon\Carbon::now($timezone);
